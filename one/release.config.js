aa299aac4e67b85529d7954cb0d3f5d4e1d3c1ad
Carlo D'Ambrosio <carlo.dambrosio@lovetoknow.com>
2023-02-21 08:23:14 +0100

    feat: initial commit

---
+++ one/release.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  branches: ['main'],
+  extends: ['semantic-release-monorepo']
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
