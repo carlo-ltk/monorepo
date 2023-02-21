ba3558775a821c886d48069d87016ff60b907830
Carlo D'Ambrosio <carlo.dambrosio@lovetoknow.com>
2023-02-21 08:27:37 +0100

    fix: semantic-release config syntax

--- two/release.config.js
+++ two/release.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 module.exports = {
   branches: ['main'],
-  extends: ['semantic-release-monorepo']
+  extends: ['semantic-release-monorepo'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
